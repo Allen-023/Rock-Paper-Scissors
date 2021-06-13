f4e8a20047674e00223112e83185ccdc35e8c57f
adam allen <allen.023@hotmail.com>
2021-06-13 19:17:01 -0400

    Add project files.

---
+++ Rock Paper Sissors/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+namespace Rock_Paper_Sissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continueOrStop = "Enter a number between 1-3 or 'Stop': ";
+            
+
+            //Prompt User
+            Console.WriteLine("Welcome to Rock Paper Sissor." + "\n" + "1 = Rock, 2 = Paper, 3 = Sissor.\n");
+            Console.WriteLine(continueOrStop);
+
+            //User input 
+            var playerOne = Console.ReadLine();
+
+            //Input validation 
+            bool inputValidation = false;
+            while (inputValidation != true)
+            {
+                if (playerOne != "1" && playerOne != "2" && playerOne != "3" & playerOne != "Stop")
+                {
+                    Console.WriteLine("\n*****Input must be a number between 1-3 or Stop. Try again.*****");
+                    playerOne = Console.ReadLine();
+                }
+                else
+                {
+                    inputValidation = true;
+
+                    //game
+                    while (playerOne != "Stop" && inputValidation == true)
+                    {
+
+                        int playerOneInt = Convert.ToInt32(playerOne);
+                        Random rand = new Random();
+                        int playerTwo = rand.Next(1, 4);
+
+                        // Display results of choices picked
+                        Console.WriteLine("\n" + "You Picked: " + playerOneInt.ToString());
+                        Console.WriteLine("Computer Picked: " + playerTwo.ToString());
+
+                        //tie
+                        if (playerOneInt == playerTwo)
+                        {
+                            Console.WriteLine("Its a tie!" + "\n\n" + continueOrStop);
+                            inputValidation = false;
+                            playerOne = Console.ReadLine();
+                        }
+                        //rock beats sissors 
+                        else if (playerOneInt == 1 && playerTwo == 3)
+                        {
+                            Console.WriteLine("You Won!" + "\n\n" + continueOrStop);
+                            inputValidation = false;
+                            playerOne = Console.ReadLine();
+                        }
+                        //paper beats rock
+                        else if (playerOneInt == 2 && playerTwo == 1)
+                        {
+                            Console.WriteLine("You Won!" + "\n\n" + continueOrStop);
+                            inputValidation = false;
+                            playerOne = Console.ReadLine();
+                        }
+                        // sissor beats paper
+                        else if (playerOneInt == 3 && playerTwo == 2)
+                        {
+                            Console.WriteLine("You Won!" + "\n\n" + continueOrStop);
+                            inputValidation = false;
+                            playerOne = Console.ReadLine();
+                        }
+                        // otherwise lose
+                        else
+                        {
+                            Console.WriteLine("Sorry, You lose." + "\n\n" + continueOrStop);
+                            inputValidation = false;
+                            playerOne = Console.ReadLine();
+                        }
+                    }
+                }
+            }
+
+
+
+            
+
+            Console.WriteLine("\nThanks for playing!");
+
+
+
+        }
+    }
+}
